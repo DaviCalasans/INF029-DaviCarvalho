084178b693aff96178727f9e56e62215560c96b3
Davi Calasans <davivida878@gmail.com>
2026-04-08 00:16:56 -0300

    fix: Modularizando menu de aluno

--- projeto_escola/projetoEscola.c
+++ projeto_escola/projetoEscola.c
@@ -13,7 +13,7 @@ typedef struct
     char cpf[12];
 } Aluno;
 
-void listar_alunos(int qtdAlunos, Aluno * listaAlunos)
+void listar_alunos(int qtdAlunos, Aluno *listaAlunos)
 {
     for (int i = 0; i < qtdAlunos; i++)
     {
@@ -26,7 +26,7 @@ void listar_alunos(int qtdAlunos, Aluno * listaAlunos)
     }
 };
 
-void busca_aluno_por_id(int qtdAlunos, Aluno * listaAlunos, int id)
+void busca_aluno_por_id(int qtdAlunos, Aluno *listaAlunos, int id)
 {
     for (int i = 0; i < qtdAlunos; i++)
     {
@@ -39,8 +39,92 @@ void busca_aluno_por_id(int qtdAlunos, Aluno * listaAlunos, int id)
             printf("Data de Nascimento aluno: %s\n", listaAlunos[i].data_nascimento);
             printf("CPF aluno: %s\n", listaAlunos[i].cpf);
             break;
-        } else {
+        }
+        else
+        {
             printf("ID %d não encontrado!", id);
+        }
+    }
+}
+
+void menuAlunos(Aluno *listaAlunos, int *qtdAlunos, int *codigoAluno, int *idAlunoEscolhido)
+{
+    int opcao = 0;
+    printf("Menu alunos \n");
+    while (opcao >= 0)
+    {
+        printf("Digite uma opção:\n");
+        printf("0 - Para voltar ao menu principal\n");
+        printf("1 - Para cadastrar aluno\n");
+        printf("2 - Para listar aluno\n");
+        printf("3 - Para atualizar aluno\n");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 0:
+            opcao = -1;
+            break;
+
+        case 1:
+            if (*qtdAlunos == TAM_LISTA_ALUNOS)
+            {
+                printf("A quantidade de alunos atingiu o limite\n");
+            }
+            else
+            {
+                listaAlunos[*qtdAlunos].id = *codigoAluno;
+
+                printf("Matricula:\n");
+                scanf("%d", &listaAlunos[*qtdAlunos].matricula);
+
+                printf("Nome:\n");
+                scanf("%s", listaAlunos[*qtdAlunos].nome);
+
+                printf("Sexo (M/F):\n");
+                scanf(" %c", &listaAlunos[*qtdAlunos].sexo);
+
+                printf("Data de Nascimento (dd/mm/aaaa):\n");
+                scanf("%s", listaAlunos[*qtdAlunos].data_nascimento);
+
+                printf("CPF (somente números):\n");
+                scanf("%s", listaAlunos[*qtdAlunos].cpf);
+
+                (*qtdAlunos)++;
+                (*codigoAluno)++;
+            }
+            break;
+
+        case 2:
+            if (*qtdAlunos == 0)
+            {
+                printf("A lista de alunos está vazia\n");
+            }
+            else
+            {
+                listar_alunos(*qtdAlunos, listaAlunos);
+            }
+            break;
+
+            case 3:
+                if (qtdAlunos == 0)
+                {
+                    printf("A lista de alunos está vazia\n");
+                }
+                else
+                {
+                    printf("Qual aluno você quer atualizar?\n");
+                    listar_alunos(*qtdAlunos, listaAlunos);
+                    scanf("%d", idAlunoEscolhido);
+                    printf("O id escolhido foi: %d", idAlunoEscolhido);
+                    busca_aluno_por_id(*qtdAlunos, listaAlunos, *idAlunoEscolhido);
+                    printf("Qual informação você quer atualizar?");
+                }
+                break;
+
+        default:
+            printf("Opção inválida\n");
+            break;
         }
     }
 }
@@ -53,80 +137,24 @@ int main()
     int codigoAluno = 1;
     int idAlunoEscolhido;
 
-    Aluno listaAlunos[TAM_LISTA_ALUNOS];
+    Aluno *listaAlunos;
+    listaAlunos = malloc(TAM_LISTA_ALUNOS * sizeof(Aluno));
 
     while (sair == 0)
     {
         printf("Digite uma opção:\n");
         printf("0 - para encerrar\n");
-        printf("1 - para cadastrar aluno\n");
-        printf("2 - para listar aluno\n");
-        printf("3 - para atualizar aluno\n");
-        scanf("%d", &opcao);
+        printf("1 - Menu aluno\n");
 
+        scanf("%d", &opcao);
         switch (opcao)
         {
         case 0:
             sair = 1;
             break;
-
         case 1:
-            if (qtdAlunos == TAM_LISTA_ALUNOS)
-            {
-                printf("A quantidade de alunos atingiu o limite\n");
-            }
-            else
-            {
-                listaAlunos[qtdAlunos].id = codigoAluno;
-
-                printf("Matricula:\n");
-                scanf("%d", &listaAlunos[qtdAlunos].matricula);
-
-                printf("Nome:\n");
-                scanf("%s", listaAlunos[qtdAlunos].nome);
-
-                printf("Sexo (M/F):\n");
-                scanf(" %c", &listaAlunos[qtdAlunos].sexo);
-
-                printf("Data de Nascimento (dd/mm/aaaa):\n");
-                scanf("%s", listaAlunos[qtdAlunos].data_nascimento);
-
-                printf("CPF (somente números):\n");
-                scanf("%s", listaAlunos[qtdAlunos].cpf);
-
-                qtdAlunos++;
-                codigoAluno++;
-            }
+            menuAlunos(listaAlunos, &qtdAlunos, &codigoAluno, &idAlunoEscolhido);
             break;
-
-        case 2:
-            if (qtdAlunos == 0)
-            {
-                printf("A lista de alunos está vazia\n");
-            }
-            else
-            {
-                listar_alunos(qtdAlunos, listaAlunos);
-            }
-            break;
-
-        case 3:
-            if (qtdAlunos == 0)
-            {
-                printf("A lista de alunos está vazia\n");
-            }
-            else
-            {
-                printf("Qual aluno você quer atualizar?\n");
-                listar_alunos(qtdAlunos, listaAlunos);
-                scanf("%d", &idAlunoEscolhido);
-                printf("O id escolhido foi: %d", idAlunoEscolhido);
-                busca_aluno_por_id(qtdAlunos, listaAlunos, idAlunoEscolhido);
-                printf("Qual informação você quer atualizar?");
-                
-            }
-            break;
-
         default:
             printf("Opção inválida\n");
             break;
