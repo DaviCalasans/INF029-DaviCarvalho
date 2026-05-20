674f42a3c1b305a3dbb1d28f06c49521f03a42cd
Davi Calasans de Sá Carvalho <144958896+DaviCalasans@users.noreply.github.com>
2026-05-20 19:11:12 -0300

    feat: Adicionando dados ao cabeçalho

--- trabalho1/trabalho1.c
+++ trabalho1/trabalho1.c
@@ -10,10 +10,10 @@
 //  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
 
 //  ----- Dados do Aluno -----
-//  Nome:
-//  email:
-//  Matrícula:
-//  Semestre:
+//  Nome: Davi Calasans de Sá Carvalho 
+//  email: 20252160021@ifba.edu.br
+//  Matrícula: 20252160021
+//  Semestre: 2° Semestre
 
 //  Copyright © 2016 Renato Novais. All rights reserved.
 // Última atualização: 07/05/2021 - 19/08/2016 - 17/10/2025
